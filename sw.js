224a0c48d8c27df4a250bb6a90f699c442499464
salehjawar <salehjawar@gmail.com>
2026-01-27 10:47:17 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'ronikacv-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css'
+];
+
+// نصب سرویس ورکر و کش کردن فایل‌ها
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// استفاده از کش در صورت نبود اینترنت
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
